4acc8ab097a4cd735996a1ab9f8b17ce5ab1b306
EnriqueMayaFernandez <enriquemayatrabajar@gmail.com>
2022-04-04 16:10:27 +0200

    correccion de la url de mongo

--- CVBack/db.js
+++ CVBack/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const urlMongo = "mongodb://127.0.0.1:27017";
+const urlMongo = "mongodb://localhost:27017/cvweb";
 
 
 
--- CVBack/routes/api/proyectos.js
+++ CVBack/routes/api/proyectos.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 
+const Proyecto = require('../../models/proyecto');
+
 router.get('/', (req,res) =>{
+    let proyecto = new Proyecto();
+    proyecto.titulo = 'Proyecto de prueba';
+    proyecto.save().then((p)=>{
+        console.log(p);
+    }).catch(err => {
+        console.log(err);
+    })
     res.send('Estoy ya donde tengo que estar');
 });
 
